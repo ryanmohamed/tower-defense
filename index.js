62020f0f9ee04ec56813ce08de68a9483cf0374d
Ryan <reyaznyc@gmail.com>
2023-04-24 22:36:19 -0400

    push

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express() 
+const path = require('path')
+
+//server files from public directory when root route accessed 
+app.use('/', express.static(__dirname + 'public'))
+
+//handle 404s with some middleware
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + '/public/404.html')
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'))
